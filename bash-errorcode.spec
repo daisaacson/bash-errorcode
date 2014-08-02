1e018e8ee4439b2080edfdc859d6db698f635b4b
daisaacson <49328302+daisaacson@users.noreply.github.com>
2014-08-02 15:30:46 +0000

    Detect if TERM supports color

--- bash-errorcode.spec
+++ bash-errorcode.spec
@@ -1,6 +1,6 @@
-Summary: Displays error code in command prompt, if return code is not 0.
+Summary: Display error codes from commands in your bash prompt
 Name: bash-errorcode
-Version: 0.2
+Version: 0.3
 Release: 0%{?dist}
 BuildArch: noarch
 License: GPLv3
@@ -10,8 +10,9 @@ Url: https://raw.githubusercontent.com/daisaacson/bash-errorcode/bash-errorcode
 Buildroot: %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
 
 %description
-bash-errorcde is a bash profile that contains a function that prints
-the error code in red, if the return code is not 0. 
+bash-errorcde is a bash profile that contains a functions that prints
+error codes from previos commands.  Know what the error codes were from
+your commands without having to echo $? or ${PIPESTATUS[*]}
 
 %prep
 %setup -q
@@ -32,8 +33,15 @@ rm -rf $RPM_BUILD_ROOT
 %attr(0644,root,root) %{_sysconfdir}/profile.d/*
 
 %changelog
+* Sat Aug 1 2014 daisaacson <49328302+daisaacson@users.noreply.github.com> - 0.3-0
+- Detect if TERM supports color
+- Add comments to script
+- Format changes to functions to match output from the type bash builtin
+- Update Spec Summary and descriptsion
+
 * Tue Jul 29 2014 daisaacson <49328302+daisaacson@users.noreply.github.com> - 0.2-0
 - Print error codes from $PIPESTATUS variable
 
 * Mon Jul 28 2014 daisaacson <49328302+daisaacson@users.noreply.github.com> - 0.1-0
 - Initial build
+- Print error code from $?
--- errorcode.sh
+++ errorcode.sh
@@ -1,35 +1,57 @@
 # Print green text
-function print_green {
-  printf '\001\033[0;32m\002%s\001\033[0m\002' $1
+print_green () 
+{ 
+    printf '\001\033[0;32m\002%s\001\033[0m\002' $1;
 }
 # Print red text
-function print_red {
-  printf '\001\033[1;31m\002%s\001\033[0m\002' $1
+print_red () 
+{ 
+    printf '\001\033[1;31m\002%s\001\033[0m\002' $1;
 }
 # Print error codes
-function ps1_error {
-  # Sum of values in $PIPESTATUS
-  sum=0; for i in ${@}; do sum=$(($sum + $i)); done
+ps1_error () 
+{ 
+    codes=${@};   # array of return codes
+    codes_sum=0;  # sum of all return codes
 
-  # If error, print codes
-  if [[ ! $sum -eq 0 ]]; then
-    num_codes=$#
-    printf ' '
-    # For each error code
-    for c in ${@}; do
-      if [[ $c -eq 0 ]]; then
-        print_green $c
-      else
-        print_red $c
-      fi
-      # If more codes, print a |
-      if [[ $num_codes -gt 1 ]]; then
-        printf '%s' "|"
-        num_codes=$(($num_codes-1))
-      fi
-    done
-    printf ' '
-  fi
+    # Get sum of all return codes
+    for c in $codes;
+    do
+        codes_sum=$((codes_sum+$c));
+    done;
+
+    # If there were errors, print codes
+    if [[ ! $codes_sum -eq 0 ]]; then
+        num_codes=$#;  # number of return codes
+        use_colors=1;  # enable colors
+
+        # Check number of colors supported
+        if [[ $(tput colors) -lt 8 ]]; then
+            use_colors=0
+        fi
+
+        # Print codes if there were errors
+        printf ' ';
+        for code in $codes;
+        do
+            # Print error code in applicable color
+            if [[ $use_colors -eq 1 ]]; then
+                if [[ $code -eq 0 ]]; then
+                    print_green $code;
+                else
+                    print_red $code;
+                fi;
+            else
+                printf '%s' $code;
+            fi;
+            # Print speerater if more codes in array
+            if [[ $num_codes -gt 1 ]]; then
+                printf '|';  # print seperater
+                num_codes=$((num_codes-1));
+            fi;
+        done;
+        printf ' ';
+    fi
 }
 
 # Set PS1 variable
